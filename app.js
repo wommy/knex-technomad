1de7801891172d8c25efe5730bec2a3e2fd75d27
wommy <tom@technomad.media>
2021-01-08 16:12:14 -0500

    pushit

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const knex = require('knex')({
-	client: 'sqlite3',
-	connection: {
-		filename: "./foobar.db"
-	},
-	useNullAsDefault: true;
-});
 
-knex.schema.createTable('users', function (table) {
-  table.increments();
-  table.string('name');
-  table.timestamps();
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('getStock').addEventListener('click', getStock);
 })
 
+function getStock(event, context, callback) {
+	// event.preventDefault();
+	console.log(event)
+
+	callback(null, {
+		statusCode: 200,
+		body: JSON.stringify( { msg: "wommy working" } )
+	})
+
+	/* later
+
+		fetch('https://ron-swanson-quotes.herokuapp.com/v2/:splat')
+			// resp sp response
+			.then((resp) => resp.json())
+			.then( (content) => {
+				let main = document.querySelector('main')
+				main.innerHTML = `<h2>foo</h2>`
+			})
+			.catch((err) => console.error)
+	
+	*/
+}
