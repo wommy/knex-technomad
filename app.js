c47cd0455ac013cd09d7dee21c06f7fc48f19272
wommy <tom@technomad.media>
2021-01-08 16:21:48 -0500

    pushit

--- app.js
+++ app.js
@@ -3,25 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('getStock').addEventListener('click', getStock);
 })
 
-function getStock(event, context, callback) {
-	// event.preventDefault();
-	console.log(event)
+function getStock(event) {
+	console.log('get')
 
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify( { msg: "wommy working" } )
-	})
+	let url = '/api/getStock'
 
-	/* later
-
-		fetch('https://ron-swanson-quotes.herokuapp.com/v2/:splat')
-			// resp sp response
-			.then((resp) => resp.json())
-			.then( (content) => {
-				let main = document.querySelector('main')
-				main.innerHTML = `<h2>foo</h2>`
-			})
-			.catch((err) => console.error)
-	
-	*/
+	fetch(url)
+		// resp sp response
+		.then((resp) => resp.json())
+		.then( (content) => {
+			let main = document.querySelector('main')
+			main.innerHTML = `<h2>foo</h2>`
+		})
+		.catch((err) => console.error)
 }
---
+++ functions/getStock.js
@@ -0,0 +1,8 @@
+exports.handler = function (event, context, callback) {
+	console.log(event);
+
+	callback(null, {
+		statusCode: 200,
+		body: JSON.stringify({ msg:"wommy working" }),
+	})
+}
