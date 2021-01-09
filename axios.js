0f15376f80517e769a9520f616aa7fd88927d306
wommy <tom@technomad.media>
2021-01-09 02:10:58 -0500

    pushit fixup

--- axios.js
+++ axios.js
@@ -1,13 +1,9 @@
+// axios
+//	.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+//	.then( res => showOutput( res ) )
+//	.catch( err => console.error( err ) )
+
 function getTodos(){
-	// axios({
-	// 	method: 'get',
-	// 	url: 'https://jsonplaceholder.typicode.com/todos',
-	// 	params: {
-	// 		_limit: 5
-	// 	}
-	// }).then(
-	// 	res => showOutput( res )
-	// ).catch( err => console.error( err ) )
 	axios
 		.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
 		.then(res => showOutput( res ) )
@@ -15,40 +11,26 @@ function getTodos(){
 }
 
 function addTodo() {
-	axios({
-		method: 'post',
-		url: 'https://jsonplaceholder.typicode.com/todos',
-		data: {
+	axios
+		.post('https://jsonplaceholder.typicode.com/todos', {
 			title: 'new todo',
 			completed: false
-		}
-	})
+		})
 		.then( res => showOutput( res ) )
 		.catch( err => console.error( err ) )
 }
 
 function showOutput(res) {
 	document.getElementById('res').innerHTML = `
-	<div>
-		<h5>status: ${res.status}</h5>
+	<div><h5>status: ${res.status}</h5></div>
+	<div><h5>headers<h5>
+		<div><pre>${JSON.stringify(res.headers, null, 2)}</pre></div>
 	</div>
-	<div>
-		<h5>headers<h5>
-		<div>
-			<pre>${JSON.stringify(res.headers, null, 2)}</pre>
-		</div>
+	<div><h5>data</h5>
+		<div><pre>${JSON.stringify(res.data, null, 2)}</pre></div>
 	</div>
-	<div>
-		<h5>data</h5>
-		<div>
-			<pre>${JSON.stringify(res.data, null, 2)}</pre>
-		</div>
-	</div>
-	<div>
-		<h5>config</h5>
-		<div>
-			<pre>${JSON.stringify(res.config, null, 2)}</pre>
-		</div>
+	<div><h5>config</h5>
+		<div><pre>${JSON.stringify(res.config, null, 2)}</pre></div>
 	</div>
 	`
 }
